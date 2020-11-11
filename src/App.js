6ea264269b6cb07c3a541ee87c393e7a5d7729b9
jito <jitokj3@gmail.com>
2020-11-11 13:02:21 +0530

    cleaned all files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AddTransaction from './components/AddTransaction';
 import Balance from './components/Balance';
 import Header from './components/Header';
 import IncomeExpense from './components/IncomeExpense';
@@ -12,6 +13,7 @@ function App() {
       <Balance />
       <IncomeExpense />
       <TransactionList />
+      <AddTransaction />
     </div>
     </div>
   );
