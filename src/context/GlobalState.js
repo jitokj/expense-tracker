ad39887e51cee737b5f2f25cfd6162c711f74ac2
jito <jitokj3@gmail.com>
2020-11-11 16:35:27 +0530

    add transaction completed

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import React,{ useReducer,createContext } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-    transactions : [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions : []
 }
 
 export const GlobalContext = createContext(initialState);
@@ -23,7 +18,14 @@ export const GlobalProvider = ({children})=>{
         })
     }
 
-    return (<GlobalContext.Provider value={{transactions: state.transactions,deleteTransaction}}>
+    function addTransaction(transaction) {
+        dispatch({
+            type: "ADD_TRANSACTION",
+            payload: transaction
+        })
+    }
+
+    return (<GlobalContext.Provider value={{transactions: state.transactions,deleteTransaction,addTransaction}}>
         {children}
     </GlobalContext.Provider>);
 }
